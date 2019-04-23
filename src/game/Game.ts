ad40ce43c1a12de2cddc244c017beb76481e7884
TahitianBlue <47165016+TahitianBlue@users.noreply.github.com>
2019-04-23 17:56:21 +0900

    不要コード削除

--- src/game/Game.ts
+++ src/game/Game.ts
@@ -2,29 +2,26 @@
 // ゲームシーン
 
 const PIXEL_PER_METER = 10;
-const PLAYER_RADIUS_PER_W = 1/40;
-const HOOK_RADIUS_PER_W = 1/60;
+const PLAYER_RADIUS_PER_W = 1/24;
+const WARD_RADIUS_PER_W = 1/24;
 const BLOCK_SIZE_PER_W = 1/20;
-const FLOOR_SIZE_PER_W = 1/3;
 const GRAVITY_PER_H = (1/300);
 
-const SAVE_KEY_BESTSCORE = "hook-bestScore";
+const SAVE_KEY_BESTSCORE = "guardian-bestScore";
 
 const BACK_COLOR = 0xF0F0F2;    // index.htmlで設定
 const FONT_COLOR = 0xE0B060;
 const PLAYER_COLOR = 0x0D697E;
-const COIN_COLOR = 0xC09030;
+const WARD_COLOR = 0x0D697E;
 const BLOCK_COLOR = 0xC0001B;
-const FLOOR_COLOR = 0x608090;
 
 class Game {
 
     static loadSceneGamePlay() {
         PhysicsObject.deltaScale = 1;
-        // new Background();
-        new Wave();
-        new StartMessage();
         new Score();
         new Player( Util.width * 0.2, Util.height * 0.2 );
+        new Wave();
+        new StartMessage();
     }
 }
--- src/game/Player.ts
+++ src/game/Player.ts
@@ -1,6 +1,5 @@
 // Liberapp 2019 - Tahiti Katagai
-// プレイヤー　押しのけ守護神
-// Player Guardian
+// プレイヤー　押しのけ守護神 Guardian
 
 class Player extends PhysicsObject{
 
@@ -9,8 +8,6 @@ class Player extends PhysicsObject{
     radius:number;
     button:Button;
     hookR:number = 0;
-    hook:Hook = null;
-    wire:Wire = null;
     state:()=>void = this.stateNone;
     step:number = 0;
 
@@ -69,70 +66,16 @@ class Player extends PhysicsObject{
 
     setStateNone(){
         this.state = this.stateNone;
-        if( this.wire ){
-            this.wire.rewind = true;
-            this.wire = null;
-        }
     }
     stateNone(){}
 
-    setStateFree(){
-        this.state = this.stateFree;
+    setStateMove(){
+        this.state = this.stateMove;
         this.step = 0;
-        this.body.gravityScale = 1;
     }
-    stateFree() {
-        this.step++;
-        let hook = Hook.detect( this.px, this.py );
-        if( hook && this.button.press ){
-            this.setStateHang( hook );
-            return;
-        }
-
-        // 転落チェック
-        if( this.body.velocity[1] > 0 && this.display.y > Util.height ){
-            this.gameOver();
-            this.setStateNone();
-        }
+    stateMove() {
     }
 
-    setStateHang( hook:Hook ){
-        this.state = this.stateHang;
-        this.hook = hook;
-        this.hook.catch();
-        this.hookR = Math.sqrt( (this.hook.px - this.px)**2 + (this.hook.py - this.py)**2 );
-        this.body.velocity[0] *= 1.2;
-        this.body.velocity[1] *= 1.2;
-        this.wire = new Wire( this.display, hook.display );
-    }
-    stateHang(){
-        // ワイヤー長さの距離
-        let dx = this.hook.px - this.px;
-        let dy = this.hook.py - this.py;
-        let d  = Math.sqrt( dx**2 + dy**2 );
-        let _d = 1 / d;
-        dx *= _d;
-        dy *= _d;
-        this.px = this.hook.px - dx * this.hookR;
-        this.py = this.hook.py - dy * this.hookR;
-        this.display.x = this.px;
-        this.display.y = this.py;
-
-        // 振り子
-        let dot = this.body.velocity[0] * dx + this.body.velocity[1] * dy;
-        this.body.velocity[0] -= dx * dot;
-        this.body.velocity[1] -= dy * dot;
-        this.body.velocity[0] *= 1.01;
-        this.body.velocity[1] *= 1.01;
-
-        if( this.button.touch == false ){
-            this.body.velocity[1] -= Util.height * 0.005;
-            this.wire.rewind = true;
-            this.wire = null;
-            this.setStateFree();
-        }
-    }
-    
     gameOver(){
         new GameOver();
         PhysicsObject.deltaScale = 0.1;
